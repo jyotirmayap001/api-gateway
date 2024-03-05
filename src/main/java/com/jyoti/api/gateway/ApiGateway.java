4c021edc2a4d315902799dd69862a22bae0d54fb
jyotirmayap001 <73281245+jyotirmayap001@users.noreply.github.com>
2024-03-05 13:51:40 +0530

    Update ApiGateway.java

--- src/main/java/com/jyoti/api/gateway/ApiGateway.java
+++ src/main/java/com/jyoti/api/gateway/ApiGateway.java
@@ -3,6 +3,9 @@ package com.jyoti.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
